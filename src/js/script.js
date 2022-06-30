47cf19c9e23cd1c5d7f8a6d98f790725443e1862
Yehor Koval <yehor1koval@gmail.com>
2022-06-30 14:06:30 +0300

    Add animation on review

--- src/js/script.js
+++ src/js/script.js
@@ -106,4 +106,23 @@ $(document).ready(function(){
       });
       return false;
   });
+
+  //стрелка скрола вверх pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  //скрипты для анимаций
+  new WOW().init();
   });
